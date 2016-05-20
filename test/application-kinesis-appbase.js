957c6f9d99de7276a7f303b1f8a4a51c5da87d1b
Rishi <rishiloyola98245@gmail.com>
2016-05-21 01:56:17 +0530

    Add the transform function

--- test/application-kinesis-appbase.js
+++ test/application-kinesis-appbase.js
@@ -1,1 +1,2 @@
-Source({name:"kinesisstream"}).save({name:"appbaseapp", namespace:"exampleapp.example", debug:true});
+Source({name:"kinesisstream"}).transform({filename: "transformers/json_converter.js", namespace: "kinesis.appbase_test", debug: true}).save({name:"appbaseapp", namespace:"test_123.appbase_test", debug:true});
+//Source({name:"kinesisstream"}).save({name:"appbaseapp", namespace:"test_123.test", debug:true});
